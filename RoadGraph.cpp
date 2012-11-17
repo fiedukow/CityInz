405026d873dd991a4e44afe6a77abbc5ee134f54
fiedukow <fiedukow@gmail.com>
2012-11-17 16:33:20 +0100

    RoadGraph interface created.

--- RoadGraph.cpp
+++ RoadGraph.cpp
@@ -1,5 +1,13 @@
 #include "RoadGraph.h"
 
+GeoCoords::GeoCoords()
+  : lat(0), lon(0)
+{}
+
+GeoCoords::GeoCoords(double lat, double lon)
+  : lat(lat), lon(lon)
+{}
+
 RoadGraph::RoadGraph()
 {
 }
--- RoadGraph.h
+++ RoadGraph.h
@@ -1,10 +1,39 @@
 #ifndef ROADGRAPH_H
 #define ROADGRAPH_H
 
+#include <map>
+#include <list>
+
+struct GeoCoords;
+
+typedef unsigned long long VertexId;
+typedef std::map<VertexId, GeoCoords> VertexMap;
+typedef std::list<VertexId, VertexId> EdgesList;
+
+struct GeoCoords
+{
+  GeoCoords();
+  GeoCoords(double lat, double lon);
+  double lat;
+  double lon;
+};
+
 class RoadGraph
 {
 public:
   RoadGraph();
+  void addVertex(VertexId id, GeoCoords coords);
+  bool addEdge(VertexId, VertexId);
+
+  VertexMap::const_iterator vertexBegin() const;
+  VertexMap::const_iterator vertexEnd() const;
+
+  EdgesList::const_iterator edgesBegin() const;
+  EdgesList::const_iterator edgesEnd() const;
+
+private:
+  VertexMap vertexes;
+  EdgesList edges;
 };
 
 #endif // ROADGRAPH_H
