0b008d6109b95adbf991e0a1f6e9c2989e3f1402
fiedukow <fiedukow@gmail.com>
2012-11-20 22:46:11 +0100

    Map uses metric coords.

--- src/CityMapFrame.cpp
+++ src/CityMapFrame.cpp
@@ -10,14 +10,14 @@ CityMapFrame::CityMapFrame(QWidget* parent, RoadGraphPtr graph)
   time_t begin = time(NULL);
   graph->normalize();
   graph->scale(0.1);
-  //int maxLon = graph->maxLon();
+
   MetricCoords maxCoords = graph->maxCoords();
   int maxY = maxCoords.y;
   int maxX = maxCoords.x;
   std::cout << maxX << " x " << maxY << std::endl;
   img = QImage(maxX, maxY, QImage::Format_RGB32);
   QPainter painter(&img);
-  //painter.setRenderHint(QPainter::Antialiasing);
+  painter.setRenderHint(QPainter::Antialiasing);
 
   QPen pen(Qt::red);
   pen.setColor(QColor::fromRgb(0x7C, 0x41, 0x41));
@@ -34,15 +34,18 @@ CityMapFrame::CityMapFrame(QWidget* parent, RoadGraphPtr graph)
   pen.setWidth(2);
   pen.setStyle(Qt::SolidLine);
   painter.setPen(pen);
-  /*
+
   for (VertexMap::const_iterator i = graph->vertexBegin();
        i != graph->vertexEnd();
        ++i)
   {
-    const GeoCoords& c = i->second;
+    const MetricCoords& c = i->second;
     //std::cout << c.lon << ", " << c.lat << std::endl;
-    //painter.drawRect(static_cast<int>(c.lon), static_cast<int>(c.lat), 2, 2);
-  }*/
+    painter.drawRect((static_cast<int>(c.x)-1),
+                     maxY-(static_cast<int>(c.y)-1),
+                     2,
+                     2);
+  }
 
   for (EdgesVector::const_iterator i = graph->edgesBegin();
        i != graph->edgesEnd();
